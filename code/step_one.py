af422d0b25e6ee567a7f0026a884da3eca444ab8
Silvia-Guidi <silvia.guidi1@studenti.unimi.it>
2025-08-20 17:24:17 +0200

    Update step_one.py

--- code/step_one.py
+++ code/step_one.py
@@ -1,20 +1,42 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from ucimlrepo import fetch_ucirepo
 wine_quality = fetch_ucirepo(id=186)
 # variable information
 print(wine_quality.variables)
+#print(wine_quality.metadata) 
 
 #data exploration
 df = pd.concat([wine_quality.data.features, wine_quality.data.targets], axis=1)
-print(df.isnull().sum())
-print(df.describe())
-print(df.corr())
+#print(df.isnull().sum())
+
+fig, axes = plt.subplots(3, 4, figsize=(16, 10))  
+axes = axes.flatten()
+for i, col in enumerate(df.columns):  
+    sns.histplot(df[col], kde=False, ax=axes[i], color="#2CEAA3")  
+    axes[i].set_title(f"Distribution of {col}", fontsize=12)
+fig.suptitle("Distribution of the variables: histograms", fontsize=16)
+plt.tight_layout()
+#plt.show()
+
+fig, axes = plt.subplots(3, 4, figsize=(16,10))  # 3 rows, 4 columns
+axes = axes.flatten()
+
+for i, col in enumerate(df.columns):
+    axes[i].boxplot(df[col], patch_artist=True, boxprops=dict(facecolor="#2CEAA3"))
+    axes[i].set_title(col)
+fig.suptitle("Distribition of the variables: boxplots", fontsize=16)
+plt.tight_layout()
+#plt.show()
+
+#print(df.describe())
+#print(df.corr())
+
 ##heatmap PLOT
-plt.figure(figsize=(10,8))
+plt.figure(figsize=(8,15))
 sns.heatmap(df.corr(), annot=True, fmt=".2f", cmap="coolwarm", cbar=True)
+plt.title("Correlation Heatmap", fontsize=16, pad=20)
 plt.show()
-##target distribution PLOT
-sns.countplot(x="quality", data=df)
-plt.show()
+
